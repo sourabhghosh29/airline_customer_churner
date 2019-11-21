997d6381fb7157a531c20208eaca3ca5d664ffb9
Jimit Mistry <mistryjimit26@gmail.com>
2019-11-20 22:07:57 -0500

    Graphs, Maps, Analysis, Apriori, Word Corpus

--- Project_final.R
+++ Project_final.R
@@ -61,7 +61,11 @@ str(df_nps$Arrival_Delay_greater_than_5)
 quantile(df_nps$Flight.Distance)
 df_nps$Long_Trip <- df_nps$Flight.Distance > 1100
 
-
+# Create Flight_time
+df_nps$flight_day_part <- cut(df_nps$Scheduled.Departure.Hour,
+                             breaks = c(-1,6,12,17,19,Inf),
+                             labels = c("late night","morning","afternoon","evening","night"))
+glimpse(df_nps)
 
 # Plots
 
@@ -77,8 +81,9 @@ ggplot(df_nps, aes(x = "",y = "", fill = type_passenger))+
 
 
 # Apriori Transactions
+glimpse(df_nps)
 dim(df_nps)
-df_nps1 = df_nps[,c(1,2,3,5,7,10,33)]
+df_nps1 = df_nps[,c(3,5,10,14,33,34,35,36)]
 
 apply(apply(df_nps1, 2, is.na), 2, which)
 df_nps1$type_passenger[which(is.na(df_nps1$type_passenger))] = "passive"
@@ -93,9 +98,24 @@ ruleset <- apriori(df_npsX,
                    # Specify threshold of 0.005 for support, and 0.5 for confidence. That is, show only those values that are higher than the thresholds.
                    parameter = list(support=0.05, confidence = 0.05, minlen = 3),
                    # Specify the rhs as "Survived = Yes", and all the rest of the variables of the transaction as lhs of the equation
-                   appearance = list(default="lhs", rhs = ("type_passenger=promoter")))
+                   appearance = list(default="lhs", rhs = ("type_passenger=detractor")))
 inspect(ruleset)
 inspectDT(ruleset)
+
+
+#Promoter rules
+#{Type.of.Travel=Business travel,Class=Eco}	{type_passenger=promoter}	0.110	0.222	1.441	1,132.000
+
+#Detractor
+# {Airline.Status=Blue,Type.of.Travel=Personal Travel}	{type_passenger=detractor}	0.213	0.886	1.997	2,186.000
+# {Airline.Status=Blue,Type.of.Travel=Personal Travel,Class=Eco}	{type_passenger=detractor}	0.174	0.885	1.995	1,791.000
+# {Gender=Female,Type.of.Travel=Personal Travel,Class=Eco,Arrival_Delay_greater_than_5}	{type_passenger=detractor}	0.055	0.928	2.091	565.000
+# {Type.of.Travel=Personal Travel,Class=Eco,Arrival_Delay_greater_than_5}	{type_passenger=detractor}	0.084	0.921	2.076	859.000
+# {Type.of.Travel=Personal Travel,Arrival_Delay_greater_than_5}	{type_passenger=detractor}	0.100	0.920	2.075	1,028.000
+# {Airline.Status=Blue,Type.of.Travel=Personal Travel,flight_day_part=afternoon}	{type_passenger=detractor}	0.072	0.903	2.036	738.000
+# {Airline.Status=Blue,Type.of.Travel=Personal Travel,Class=Eco,flight_day_part=afternoon}	{type_passenger=detractor}	0.059	0.902	2.034	610.000
+# {Airline.Status=Blue,Gender=Female,Type.of.Travel=Personal Travel}	{type_passenger=detractor}	0.140	0.897	2.021	1,441.000
+
 
 boxplot(df_nps$Age ~ df_nps$type_passenger)
 
@@ -213,8 +233,39 @@ ggplot(df_nps, aes(Shopping.Amount.at.Airport))+
 ggplot(df_nps, aes(Eating.and.Drinking.at.Airport))+
   geom_histogram(aes(fill = type_passenger), bins = 10, color = 'Black')
 
+ggplot(df_nps, aes(flight_day_part))+
+  geom_histogram(aes(fill = type_passenger), bins = 10, color = 'Black', stat = "count")
 
-# Fins important words from freeText
+ggplot(df_nps, aes(flight_day_part))+
+  geom_histogram(aes(fill = Arrival_Delay_greater_than_5), bins = 10, color = 'Black', stat = "count")
+
+ggplot(df_nps, aes(type_passenger))+
+  geom_histogram(aes(fill = Arrival_Delay_greater_than_5), bins = 10, color = 'Black', stat = "count")
+
+# map plt
+us <- map_data("state")
+df_nps$Origin.State <- tolower(df_nps$Origin.State)
+df_nps$Destination.State <- tolower(df_nps$Destination.State)
+
+flight_state_destination <- ggplot(df_nps, aes(map_id = Destination.State))+
+  geom_map(map = us, aes(fill = type_passenger), color = 'Black')+
+  expand_limits(x = us$long, y = us$lat)+
+  #geom_point(aes(x = dlong, y = dlat))+
+  ggtitle("Destination State")+
+  coord_map()
+flight_state_destination
+
+flight_state_origin <- ggplot(df_nps, aes(map_id = Origin.State))+
+  geom_map(map = us, aes(fill = type_passenger), color = 'Black')+
+  expand_limits(x = us$long, y = us$lat)+
+  #geom_point(aes(x = olong, y = olat))+
+  ggtitle("Origin State")+
+  coord_map()
+flight_state_origin
+
+
+
+# Find important words from freeText
 which(!is.na(df_nps$freeText))
 comments <- df_nps$freeText[which(!is.na(df_nps$freeText))]
 
@@ -230,7 +281,7 @@ comments.corpus <- tm_map(comments.corpus, removeWords, stopwords("english"))
 
 inspect(comments.corpus)
 
-as(comments.corpus, "transactions")
+#as(comments.corpus, "transactions")
 
 tdm <- TermDocumentMatrix(comments.corpus)
 
